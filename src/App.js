9c68e293e700d280c5a6c4919fe8083d9a12b684
Anthony Ajami <95594568+JamonCyberico@users.noreply.github.com>
2024-06-21 20:10:56 +0200

    add testimonials

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import About from "./pages/About";
 import Work from "./pages/Work";
 import Navbar from "./components/Navbar";
@@ -6,6 +6,7 @@ import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
 import { ThemeProvider } from "./context/ThemeContext";
 import TechNews from "./pages/TechNews";
+import Testimonials from "./pages/Testimonials";
 
 function App() {
   return (
@@ -22,10 +23,12 @@ function App() {
                   <Skills />
                   <Work />
                   <Contact />
+                  <Testimonials />
                 </>
               }
             />
             <Route path="/news" element={<TechNews />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </div>
       </ThemeProvider>
